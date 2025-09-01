ca2db9238b0f6440d073c6b4e8fc55a03ebedb56
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-09-01 08:33:29 -0300

    header of macros and types files

---
+++ scatter_open_addressing/Tipos.h
@@ -0,0 +1,59 @@
+/****
+ *
+ * Arquivo Tipos.h
+ *
+ * Autor: Ulysses de Oliveira
+ *
+ * Data de Criação: 07/06/2010
+ * Última modificação: 07/06/2010
+ *
+ * Descrição: Tipos utilizados pelo programa de dispersão com endereçamento aberto
+ *
+ ****/
+
+#ifndef _Tipos_H_ /* Previne inclusão múltipla deste arquivo */
+#define _Tipos_H_
+
+#include "Macros.h"  /* Macros usadas pelo programa */
+
+typedef char tCEP[TAM_CEP + 1]; /* Tipo da chave */
+
+   /* Tipo dos pares chave/índice */
+typedef struct  {
+           tCEP chave;  /* Chave de um registro */
+           int  indice; /* Índice do registro no arquivo */
+        } tCEP_Ind;
+
+   /* Tipo usado para indicar o status */
+   /* de um elemento da tabela         */
+typedef enum {VAZIO, OCUPADO, REMOVIDO} tStatusDEA;
+
+   /* Tipo dos elementos da tabela de busca */
+typedef struct {
+           tCEP_Ind   chaveEIndice;
+           tStatusDEA  status;
+        } tColetorDEA, *tTabelaDEA;
+
+   /* Tipo de um ponteiro para uma função de */
+   /* dispersão com endereçamento aberto     */
+typedef unsigned (*tFDispersaoDEA) (tCEP, unsigned, unsigned);
+
+   /* Tipo usado para armazenamento de */
+   /* registros no arquivo binário     */
+typedef struct {
+           int  numero;
+           char UF[TAM_UF];
+           int  localidadeNumero;
+           char nomeAbr[MAX_NOME];
+           char nome[MAX_NOME];
+           int  bairroInicio;
+           int  bairroFim;
+           char CEP[TAM_CEP];
+           char complemento[MAX_COMP];
+           char tipoLogradouro[MAX_TIPO_LOG];
+           char statusTipoLogradouro;
+           char nomeSemAcento[MAX_NOME];
+           char chaveDNE[TAM_DNE];
+         } tRegistroCEP;
+
+#endif
