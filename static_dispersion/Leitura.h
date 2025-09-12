b50aa7f1ff2ff640de670144b27b84712f0b9977
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-09-12 09:53:24 -0300

    Add implementation of LeituraFacil library with input functions in Leitura.c and Leitura.h

---
+++ static_dispersion/Leitura.h
@@ -0,0 +1,33 @@
+/****
+ *
+ * Título: LeituraFacil
+ *
+ * Autor: Ulysses de Oliveira
+ *
+ * Data de Criação: 15/07/2012
+ * Última modificação: 15/07/2012
+ *
+ * Descrição: Definição do módulo da biblioteca LeituraFacil
+ *
+ ****/
+
+#ifndef _Leitura_H_
+#define _Leitura_H_
+
+extern char *LeLinhaIlimitada(int *tam, FILE *stream);
+extern int LeCaractere(void);
+extern int LeInteiro(void);
+extern int LeNatural(void);
+extern int LeNaturalPositivo(void);
+extern int LeInteiroEntre(int valor1, int valor2);
+extern double LeReal(void);
+extern int LeString(char *ar, int nElementos);
+extern int LeOpcao(const char *opcoes);
+extern int LeNome(char *nome, int tam);
+extern char *LeMatricula( const char *nomeMatr, char *matr,
+                          int tamArray );
+extern double LeNota(void);
+extern long LeChave(void);
+extern int LeSimNao(const char *prompt);
+
+#endif
