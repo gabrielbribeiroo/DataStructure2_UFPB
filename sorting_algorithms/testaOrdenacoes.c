edb6df82e6d766965e90bb97958692a0962749ca
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-09-19 13:06:15 -0300

    Add RadixSort implementation

--- sorting_algorithms/testaOrdenacoes.c
+++ sorting_algorithms/testaOrdenacoes.c
@@ -340,3 +340,42 @@ void BucketSort(unsigned char ar[], int t){
         }
     }
 }
+
+void RadixSort(unsigned char ar[], int t){
+    int maior, i, j, idx;
+
+    //procurar maior valor
+    maior = ar[0];
+    for(i = 1; i < t; i++){
+        if (ar[i] > maior){
+            maior = ar[i];
+        }
+    }
+
+    unsigned char lista[10][t];
+    int qLista[10] = {0};
+
+    idx = 1;
+    int l, k = 0;
+    while(maior/idx > 0){
+        //distribuicao
+        for(i = 0; i < t; i++){
+            l = ar[i]/idx % 10;
+            lista[l][qLista[l]] = ar[i];
+            qLista[l]++;
+        }
+
+        //recuperacao
+        k = 0;
+        for(i = 0; i < 10; i++){
+            for(j = 0; j < qLista[i]; j++){
+                ar[k++] = lista[i][j];
+            }
+        }
+        //zera listas
+        memset(qLista, 0, sizeof(int)*10);
+
+        idx *= 10;
+    }
+
+}
