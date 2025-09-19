42383a11038bffb8e7c15b5840e4b4e52a5cd7e0
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-09-19 13:04:56 -0300

    Add CountingSort implementation to sorting algorithms

--- sorting_algorithms/testaOrdenacoes.c
+++ sorting_algorithms/testaOrdenacoes.c
@@ -267,3 +267,41 @@ void HeapSort(unsigned char ar[], int t){
         OrdenaHeap(ar, 0, i);
     }
 }
+
+void CountingSort(unsigned char ar[], int t){
+    int maior, i;
+
+    //procurar maior valor
+    maior = ar[0];
+    for(i = 1; i < t; i++){
+        if (ar[i] > maior){
+            maior = ar[i];
+        }
+    }
+
+    //aloca contagem
+    int tContagem = maior+1;
+    int contagem[tContagem];
+    memset(contagem, 0, sizeof(int)*(tContagem));
+
+    //conta ocorrencias
+    for(i = 0; i < t; i++){
+        contagem[ar[i]]++;
+    }
+    //ajusta contagem
+    for(i = 1; i < tContagem; i++){
+        contagem[i] += contagem[i-1];
+    }
+
+    //aloca ordenacao e preenche
+    unsigned char ordenado[t];
+    for(i = t-1; i >= 0; i--){
+        ordenado[contagem[ar[i]]-1] = ar[i];
+        contagem[ar[i]]--;
+    }
+
+    //copia para a tabela orig
+    for(i = 0; i < t; i++){
+        ar[i] = ordenado[i];
+    }
+}
